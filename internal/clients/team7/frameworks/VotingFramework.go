bb6b093035ff55b7fd5e79bdc5106af6a7901b51
daracos <daracostelloe@hotmail.com>
2023-11-22 18:14:50 +0000

    Added VoteToKickWrapper

--- internal/clients/team7/frameworks/VotingFramework.go
+++ internal/clients/team7/frameworks/VotingFramework.go
@@ -45,16 +45,19 @@ func (vf *VotingFramework) GetDecision(inputs VoteInputs) Vote {
 }
 
 func (vf *VotingFramework) deliberateVote(voteInputs VoteInputs) Vote {
+	var vote Vote
 	if voteInputs.decisionType == VoteToKickAgent {
 		// TODO: Deliberate on whether to kick an agent
 		fmt.Println("Deliberating on whether to kick an agent")
+		vote = VoteToKickWrapper(voteInputs)
 	} else if voteInputs.decisionType == VoteToAcceptNewAgent {
 		// TODO: Deliberate on whether to accept a new agent
 		fmt.Println("Deliberating on whether to accept a new agent")
+		vote = Vote{result: Map{"decision": true}}
 	} else {
 		// TODO: Deliberate on something else
 		fmt.Println("Deliberating on something else")
+		vote = Vote{result: Map{"decision": true}}
 	}
-
-	return Vote{result: Map{"decision": true}}
+	return vote
 }
