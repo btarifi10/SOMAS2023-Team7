f67d743d22bfa3b4db8f450d31f8693d752d9618
allysonburba <allyson.burba@gmail.com>
2023-11-29 09:43:41 +0000

    basic vote proposals implemented

--- internal/clients/team7/frameworks/VoteProposals.go
+++ internal/clients/team7/frameworks/VoteProposals.go
@@ -1,42 +1,45 @@
 package frameworks
 
-/*
 import (
 	"fmt"
-	"github.com/google/uuid"
 )
 
 // Ally needs the infrastructure to be updated for this to work
 func VoteOnProposalsWrapper(voteInputs VoteInputs) Vote {
-	var vote Vote
+	var vote map[interface{}]interface{}
 	switch voteInputs.VoteParameters {
 	case Proportion:
 		vote = Proportions(voteInputs)
-	case YesNo:
-		vote = YesNos(voteInputs)
+	// TODO: Implement other voting possibilities
+	//case YesNo:
+	//vote = YesNos(voteInputs)
 	default:
 		fmt.Println("New decision type!")
 		vote = Proportions(voteInputs)
 	}
 
-	return vote
+	return Vote{result: vote}
 }
 
 // TODO: Add functions for voting on which loot box to go to.
 
-
-func Proportions(voteInputs VoteInputs) Vote {
-	var votes Map
-	var candidates []uuid.UUID
-	biker *BaseTeamSevenBiker
+func Proportions(voteInputs VoteInputs) map[interface{}]interface{} {
+	var candidates []interface{}
+	var votes map[interface{}]interface{}
 	candidates = voteInputs.Candidates
 	totOptions := len(candidates)
 	normalDist := 1.0 / float64(totOptions)
-	for _, proposal := range candidates {
-		if(proposal = biker.NearestLoot())
-		votes[proposal] = normalDist
+	//nearestLoot := GetNearestLootBox()
+	for _, proposal_id := range candidates {
+		/*
+			// Basic implementation: only vote for nearest lootbox
+			if proposal_id == GetNearestLootBox() {
+				votes[proposal_id] = 1
+			} else {
+				votes[proposal_id] = 0
+			}
+		*/
+		votes[proposal_id] = normalDist
 	}
 	return votes
-
 }
-*/
